d54246a835f444c626f83602a39a8cbef3a6a6d4
Riku Rouvila <riku.rouvila@gmail.com>
2013-02-07 22:22:00 +0200

    new module

---
+++ modules/served.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+class Module:
+  def __init__(self):
+    self.module_name = 'served'
+
+  def on_message(self, msg):
+    if msg.Body == 'ei mitää' || msg.Body == 'ei mitään':
+      msg.Chat.SendMessage('(*) SERVED (*)')
